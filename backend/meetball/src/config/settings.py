d4401695eb449afda432bce9a4df0d4c815fc0ad
undergroundenemy616 <undergroundenemy616@yandex.ru>
2024-12-04 16:49:31 +0300

    [LINTUP-9](https://tracker.yandex.ru/LINTUP-9): check link

--- backend/meetball/src/config/settings.py
+++ backend/meetball/src/config/settings.py
@@ -3,9 +3,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-7d66pb6(#slc80#bxw_puvg$-m*w8ri=hr+%=pz_$*swy0vmi-"
 
